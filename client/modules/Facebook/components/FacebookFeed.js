b36ed077c7a38a7c16544900d3084319f95e5114
Nikola Stefanovic <nikolla@elfak.rs>
2018-01-21 14:52:29 +0100

    Added subcomponent FacebookComment

--- client/modules/Facebook/components/FacebookFeed.js
+++ client/modules/Facebook/components/FacebookFeed.js
@@ -6,27 +6,36 @@ var imgStyle = {
 };
 
 function FacebookImage(props) {
-    const post = props.post;
-    if (post.images && post.images.length > 0) {
-        return <img src={post.images[0].source} style={imgStyle} />
+    const images = props.images;
+    if (images && images.length > 0) {
+        return <img src={images[0].source} style={imgStyle} />
     } else {
         return null;
     }
 }
 
+function FacebookComment(props) {
+    const post = props.post;
+    return (
+        <div>
+            {post.message}
+        </div>
+    );
+}
+
 export function FacebookFeed(props) {
     const posts = props.posts;
 
-    const list = posts.map((post, i) =>
-        <div key={i}>
-            <li> {post.message} </li>
-            <FacebookImage post={post}></FacebookImage>
-        </div>
+    const feed = posts.map((post, i) =>
+        <li key={i}>
+            <FacebookComment post={post}></FacebookComment>
+            <FacebookImage images={post.images}></FacebookImage>
+        </li>
     );
 
     return (
         <ul>
-            {list}
+            {feed}
         </ul>
     );
 }   
